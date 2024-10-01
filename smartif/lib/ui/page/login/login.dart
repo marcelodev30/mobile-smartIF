c067cdefaf5beab1dd4cb575218fdd9af0b64a66
macelodev23 <marcelo-oliveira.mo@academico.ifpb.edu.br>
2024-09-30 23:44:34 -0300

    add page login

--- smartif/lib/ui/page/login/login.dart
+++ smartif/lib/ui/page/login/login.dart
@@ -10,6 +10,6 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
